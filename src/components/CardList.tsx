fd1a1d7fb3c171e5c6fe69ce941211cedad0cbe1
Luis Fernando <luis.silva9902@gmail.com>
2022-09-15 11:34:23 -0300

    Data insertion in FaunaDB and data consumption by NEXTjs API

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -21,10 +21,17 @@ export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO SELECTED IMAGE URL STATE
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(urlImage: string){
+    console.log(urlImage)
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+
+      {cards && cards.map(element => (
+        <Card data={element} viewImage={handleViewImage} key={element.id}/>
+      ))}
 
       {/* TODO MODALVIEWIMAGE */}
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Box } from '@chakra-ui/react';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
@@ -9,6 +9,15 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 export default function Home(): JSX.Element {
+
+  // useEffect(() => {
+  //   api.post('images', {
+  //     url: 'https://blog.portalpos.com.br/app/uploads/2021/08/cores.jpg',
+  //     title: 'Bexigas',
+  //     description: 'Testando a inserção no BD'
+  //   })
+  // }, [])
+
   const {
     data,
     isLoading,
@@ -19,12 +28,32 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
+    async ({ pageParam = null }) => {
+      const response = await api.get('images', {
+        params: {
+          after: pageParam
+        }
+      })
+      return response
+    }
     ,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: (response) => {
+        response.data.after ? response.data.after : null
+      }
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    if (data) {
+      const user = data.pages.map(item => {
+        return item.data.data
+      })
+      return user.flat(1)
+    }
+
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
